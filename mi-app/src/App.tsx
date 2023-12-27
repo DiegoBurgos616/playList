8123ab84013e3ea69dfec86c9c5e94c7f6a85887
Diego Burgos <A20216388@alumnos.uady.mx>
2023-12-27 14:15:50 -0600

    Se agregaron componentes (Counter y usuario)

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -1,7 +1,14 @@
+import { Counter } from "./components/Counter";
+import { Usuario } from "./components/Usuario";
+
 function App() {
   return (
     <>
    <h1>Recat + Typescript</h1>
+  <hr />
+
+<Counter/>
+<Usuario/>
     </>
   );
 }
