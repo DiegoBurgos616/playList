31b06858a0d155819d7f280d03981a2aee1871e3
Diego Burgos <A20216388@alumnos.uady.mx>
2023-12-28 15:09:18 -0600

    Se agrega formulario y un hook de este mismo para hacer mas facil su reutilizacion

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -1,26 +1,32 @@
 import { ContadorRed } from "./components/ContadorRed";
 import { Counter } from "./components/Counter";
+import { Formulario } from "./components/Formulario";
 import { TimerPadre } from "./components/TimerPadre";
 import { Usuario } from "./components/Usuario";
 
 function App() {
   return (
     <>
-   <h1>Recat + Typescript</h1>
-  <hr />
+      <h1>Recat + Typescript</h1>
+      <hr />
+      <Counter />
+      <Usuario />
 
-<Counter/>
-<Usuario/>
+      <h2>useEffect - useRef</h2>
+      <hr />
+      <TimerPadre />
 
-<h2>useEffect - useRef</h2>
-<hr />
+      <h2>useReducer</h2>
+      <hr />
+      <ContadorRed />
 
-<TimerPadre/>
+      <h2>CustomHooks</h2>
+      <Formulario />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
-<h2>useReducer</h2>
-<hr />
-<ContadorRed/>
     </>
+
+
   );
 }
 
